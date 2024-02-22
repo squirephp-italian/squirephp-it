92cc28a12bb77b39d9b560789bc5e2186a043af2
Gennaro Landolfi <code@rinodrummer.dev>
2024-02-22 17:48:47 +0100

    Updated monorepo-split action, downgraded monorepo-builder for better compatibility with Actions

--- monorepo-builder.php
+++ monorepo-builder.php
@@ -1,49 +1,47 @@
 <?php
     
-    use Symplify\MonorepoBuilder\Config\MBConfig;
+    use Symplify\MonorepoBuilder\ValueObject\Option;
     use Symplify\MonorepoBuilder\Release\ReleaseWorker\PushNextDevReleaseWorker;
     use Symplify\MonorepoBuilder\Release\ReleaseWorker\PushTagReleaseWorker;
-    use Symplify\MonorepoBuilder\ComposerJsonManipulator\ValueObject\ComposerJsonSection;
+    use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
     use Symplify\MonorepoBuilder\Release\ReleaseWorker\SetCurrentMutualDependenciesReleaseWorker;
     use Symplify\MonorepoBuilder\Release\ReleaseWorker\SetNextMutualDependenciesReleaseWorker;
     use Symplify\MonorepoBuilder\Release\ReleaseWorker\TagVersionReleaseWorker;
     use Symplify\MonorepoBuilder\Release\ReleaseWorker\UpdateBranchAliasReleaseWorker;
     use Symplify\MonorepoBuilder\Release\ReleaseWorker\UpdateReplaceReleaseWorker;
     
-    return static function (MBConfig $mbConfig): void {
-        $mbConfig->defaultBranch('main');
+    return static function (ContainerConfigurator $containerConfigurator): void {
+        $parameters = $containerConfigurator->parameters();
         
-        $mbConfig->packageDirectories([
-            __DIR__ . '/packages',
-        ]);
+        $parameters->set(Option::DEFAULT_BRANCH_NAME, 'main');
         
-        $mbConfig->dataToAppend([
-            ComposerJsonSection::AUTOLOAD => [
+        $parameters->set(Option::DATA_TO_APPEND, [
+            'autoload' => [
                 'psr-4' => [
                     'Squire\\' => 'src',
                 ],
             ],
-            ComposerJsonSection::REQUIRE => [
+            'require' => [
                 'php' => '^8.0',
                 'illuminate/support' => '^8.0|^9.0|^10.0',
                 'squirephp/repository' => '^3.4',
             ],
-            ComposerJsonSection::MINIMUM_STABILITY => 'dev',
-            ComposerJsonSection::PREFER_STABLE => true,
+            'minimum-stability' => 'dev',
+            'prefer-stable' => true,
         ]);
         
-        $mbConfig->dataToRemove([
-            ComposerJsonSection::MINIMUM_STABILITY => '*',
+        $parameters->set(Option::DATA_TO_REMOVE, [
+            'minimum-stability' => '*',
         ]);
+        
+        $services = $containerConfigurator->services();
         
         # Release workers - in order to execute
-        $mbConfig->workers([
-            UpdateReplaceReleaseWorker::class,
-            SetCurrentMutualDependenciesReleaseWorker::class,
-            TagVersionReleaseWorker::class,
-            PushTagReleaseWorker::class,
-            SetNextMutualDependenciesReleaseWorker::class,
-            UpdateBranchAliasReleaseWorker::class,
-            PushNextDevReleaseWorker::class,
-        ]);
+        $services->set(UpdateReplaceReleaseWorker::class);
+        $services->set(SetCurrentMutualDependenciesReleaseWorker::class);
+        $services->set(TagVersionReleaseWorker::class);
+        $services->set(PushTagReleaseWorker::class);
+        $services->set(SetNextMutualDependenciesReleaseWorker::class);
+        $services->set(UpdateBranchAliasReleaseWorker::class);
+        $services->set(PushNextDevReleaseWorker::class);
     };
