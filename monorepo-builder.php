b5c35a6396c98a1e6aa10feedd352be0943b9f35
Gennaro Landolfi <code@rinodrummer.dev>
2024-03-11 12:29:50 +0100

    prepare release

--- monorepo-builder.php
+++ monorepo-builder.php
@@ -1,47 +1,47 @@
 <?php
-    
-    use Symplify\MonorepoBuilder\ValueObject\Option;
-    use Symplify\MonorepoBuilder\Release\ReleaseWorker\PushNextDevReleaseWorker;
-    use Symplify\MonorepoBuilder\Release\ReleaseWorker\PushTagReleaseWorker;
-    use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
-    use Symplify\MonorepoBuilder\Release\ReleaseWorker\SetCurrentMutualDependenciesReleaseWorker;
-    use Symplify\MonorepoBuilder\Release\ReleaseWorker\SetNextMutualDependenciesReleaseWorker;
-    use Symplify\MonorepoBuilder\Release\ReleaseWorker\TagVersionReleaseWorker;
-    use Symplify\MonorepoBuilder\Release\ReleaseWorker\UpdateBranchAliasReleaseWorker;
-    use Symplify\MonorepoBuilder\Release\ReleaseWorker\UpdateReplaceReleaseWorker;
-    
-    return static function (ContainerConfigurator $containerConfigurator): void {
-        $parameters = $containerConfigurator->parameters();
-        
-        $parameters->set(Option::DEFAULT_BRANCH_NAME, 'main');
-        
-        $parameters->set(Option::DATA_TO_APPEND, [
-            'autoload' => [
-                'psr-4' => [
-                    'Squire\\' => 'src',
-                ],
+
+use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
+use Symplify\MonorepoBuilder\Release\ReleaseWorker\PushNextDevReleaseWorker;
+use Symplify\MonorepoBuilder\Release\ReleaseWorker\PushTagReleaseWorker;
+use Symplify\MonorepoBuilder\Release\ReleaseWorker\SetCurrentMutualDependenciesReleaseWorker;
+use Symplify\MonorepoBuilder\Release\ReleaseWorker\SetNextMutualDependenciesReleaseWorker;
+use Symplify\MonorepoBuilder\Release\ReleaseWorker\TagVersionReleaseWorker;
+use Symplify\MonorepoBuilder\Release\ReleaseWorker\UpdateBranchAliasReleaseWorker;
+use Symplify\MonorepoBuilder\Release\ReleaseWorker\UpdateReplaceReleaseWorker;
+use Symplify\MonorepoBuilder\ValueObject\Option;
+
+return static function (ContainerConfigurator $containerConfigurator): void {
+    $parameters = $containerConfigurator->parameters();
+
+    $parameters->set(Option::DEFAULT_BRANCH_NAME, 'main');
+
+    $parameters->set(Option::DATA_TO_APPEND, [
+        'autoload' => [
+            'psr-4' => [
+                'Squire\\' => 'src',
             ],
-            'require' => [
-                'php' => '^8.0',
-                'illuminate/support' => '^8.0|^9.0|^10.0',
-                'squirephp/repository' => '^3.4',
-            ],
-            'minimum-stability' => 'dev',
-            'prefer-stable' => true,
-        ]);
-        
-        $parameters->set(Option::DATA_TO_REMOVE, [
-            'minimum-stability' => '*',
-        ]);
-        
-        $services = $containerConfigurator->services();
-        
-        # Release workers - in order to execute
-        $services->set(UpdateReplaceReleaseWorker::class);
-        $services->set(SetCurrentMutualDependenciesReleaseWorker::class);
-        $services->set(TagVersionReleaseWorker::class);
-        $services->set(PushTagReleaseWorker::class);
-        $services->set(SetNextMutualDependenciesReleaseWorker::class);
-        $services->set(UpdateBranchAliasReleaseWorker::class);
-        $services->set(PushNextDevReleaseWorker::class);
-    };
+        ],
+        'require' => [
+            'php' => '^8.0',
+            'illuminate/support' => '^8.0|^9.0|^10.0|^11.0',
+            'squirephp/repository' => '^3.4',
+        ],
+        'minimum-stability' => 'dev',
+        'prefer-stable' => true,
+    ]);
+
+    $parameters->set(Option::DATA_TO_REMOVE, [
+        'minimum-stability' => '*',
+    ]);
+
+    $services = $containerConfigurator->services();
+
+    # Release workers - in order to execute
+    $services->set(UpdateReplaceReleaseWorker::class);
+    $services->set(SetCurrentMutualDependenciesReleaseWorker::class);
+    $services->set(TagVersionReleaseWorker::class);
+    $services->set(PushTagReleaseWorker::class);
+    $services->set(SetNextMutualDependenciesReleaseWorker::class);
+    $services->set(UpdateBranchAliasReleaseWorker::class);
+    $services->set(PushNextDevReleaseWorker::class);
+};
