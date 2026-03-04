b0ebc4a2eeccb6685af8903816680c830e8fe403
Gennaro Landolfi <code@rinodrummer.dev>
2026-03-04 21:42:56 +0100

    Fixed relations not returning the dynamic relationship

--- packages/countries-it/src/CountriesItServiceProvider.php
+++ packages/countries-it/src/CountriesItServiceProvider.php
@@ -14,7 +14,7 @@ class CountriesItServiceProvider extends ServiceProvider
         Repository::registerSource(ItCountryData::class, 'it', __DIR__ . '/../resources/it_data.csv');
 
         Country::resolveRelationUsing('itData', function (Country $countryModel) {
-            $countryModel->hasOne(ItCountryData::class, 'country_id', 'id');
+            return $countryModel->hasOne(ItCountryData::class, 'country_id', 'id');
         });
     }
 }
